374371375c535b54c805e9b6aec7a8497c038dfe
hungerdeng <hungerdeng@gmail.com>
2024-09-19 01:18:47 +0800

    feat(config): eslint,prettier,husky,commitlint config

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -1,9 +1,10 @@
-import type { Config } from "tailwindcss";
-import sharedConfig from "@repo/tailwind-config";
+import type { Config } from 'tailwindcss';
 
-const config: Pick<Config, "prefix" | "presets" | "content"> = {
-  content: ["./src/**/*.tsx"],
-  prefix: "ui-",
+import sharedConfig from '../../tailwind.config';
+
+const config: Pick<Config, 'prefix' | 'presets' | 'content'> = {
+  content: ['./src/**/*.tsx'],
+  prefix: 'ui-',
   presets: [sharedConfig],
 };
 
