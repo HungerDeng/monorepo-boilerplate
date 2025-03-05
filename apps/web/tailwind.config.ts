da4178fc1e70d3cd4ffadc389ac08c75baf7cdea
hungerdeng <hungerdeng@gmail.com>
2025-03-05 11:42:27 +0800

    feat: app/web tailwind config

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -5,7 +5,8 @@ import type { Config } from 'tailwindcss';
 import sharedConfig from '../../tailwind.config';
 
 const config: Pick<Config, 'content' | 'presets'> = {
-  content: ['./src/app/**/*.tsx'],
+  // !!! warning: property content must contain all the files under the src folder, especially shadcn folder, otherwise shadcn will not work
+  content: ['./src/**/*.tsx'],
   presets: [sharedConfig],
 };
 
